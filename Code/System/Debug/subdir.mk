9a8de5474ae2fcf7966cab19bb67930d44cfde4d
unknown <phoang13@student.aau.dk>
2016-04-21 08:39:14 +0200

    new code project folder

---
+++ Code/System/Debug/subdir.mk
@@ -0,0 +1,70 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ezdsp_setup.c \
+../main.c 
+
+ASM_SRCS += \
+../FIR.asm 
+
+CMD_SRCS += \
+../lnkx.cmd 
+
+ASM_DEPS += \
+./FIR.pp 
+
+OBJS += \
+./FIR.obj \
+./ezdsp_setup.obj \
+./main.obj 
+
+C_DEPS += \
+./ezdsp_setup.pp \
+./main.pp 
+
+OBJS__QTD += \
+".\FIR.obj" \
+".\ezdsp_setup.obj" \
+".\main.obj" 
+
+ASM_DEPS__QTD += \
+".\FIR.pp" 
+
+C_DEPS__QTD += \
+".\ezdsp_setup.pp" \
+".\main.pp" 
+
+ASM_SRCS_QUOTED += \
+"../FIR.asm" 
+
+C_SRCS_QUOTED += \
+"../ezdsp_setup.c" \
+"../main.c" 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+FIR.obj: ../FIR.asm $(GEN_OPTS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: Compiler'
+	"C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/C5500 Code Generation Tools 4.3.9/bin/cl55" -vcpu:3.3 -g --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/C5500 Code Generation Tools 4.3.9/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="FIR.pp" $(GEN_OPTS_QUOTED) $(subst #,$(wildcard $(subst $(SPACE),\$(SPACE),$<)),"#")
+	@echo 'Finished building: $<'
+	@echo ' '
+
+ezdsp_setup.obj: ../ezdsp_setup.c $(GEN_OPTS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: Compiler'
+	"C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/C5500 Code Generation Tools 4.3.9/bin/cl55" -vcpu:3.3 -g --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/C5500 Code Generation Tools 4.3.9/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="ezdsp_setup.pp" $(GEN_OPTS_QUOTED) $(subst #,$(wildcard $(subst $(SPACE),\$(SPACE),$<)),"#")
+	@echo 'Finished building: $<'
+	@echo ' '
+
+main.obj: ../main.c $(GEN_OPTS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: Compiler'
+	"C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/C5500 Code Generation Tools 4.3.9/bin/cl55" -vcpu:3.3 -g --include_path="C:/Program Files (x86)/Texas Instruments/ccsv4/tools/compiler/C5500 Code Generation Tools 4.3.9/include" --diag_warning=225 --ptrdiff_size=16 --memory_model=large --preproc_with_compile --preproc_dependency="main.pp" $(GEN_OPTS_QUOTED) $(subst #,$(wildcard $(subst $(SPACE),\$(SPACE),$<)),"#")
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Code/System/FIR.asm
@@ -0,0 +1,38 @@
+	.global _FIR, _FIRNew,_IncrementCNT;
+	
+	; int16 *DataIn 	--> AR0
+	; int16 *b_coeffs 	--> AR1
+	; int i 			--> T0
+_FIR	
+	MOV #23, BK03				; Set buffer size to filter order
+	MOV XAR0,XAR2				; CDP as coefficient pointer
+	MOV mmap(AR0),BSA23        	; Set up base address for CDP
+	MOV T0,AR2					; Start from the i'th data
+	bset AR2LC					; Set pointer as circular
+	RPT #23
+	MAC *AR2+,*AR1+,AC1 
+	SFTL AC1, #-15
+	mov AC1,T0     ; Store the rounded & scaled result
+	RET
+	
+_FIRNew	
+	MOV #23, BK03				; Set buffer size to filter order
+	MOV XAR0,XAR2				; CDP as coefficient pointer
+	MOV mmap(AR0),BSA23        	; Set up base address for CDP
+	MOV T0,AR2					; Start from the i'th data
+	bset AR2LC					; Set pointer as circular
+	RPT #23
+	MAC *AR2+,*AR1+,AC1 
+	SFTL AC1, #-15
+	mov AC1,T0     ; Store the rounded & scaled result
+	RET
+	
+;_IncrementCNT
+;	ADD #1,*(AR0)
+;	CALLCC ResetCNT,AC1>=#2000h
+;	RET
+	
+;ResetCNT
+;	MOV #0, *(AR0)
+;	RET
+	
